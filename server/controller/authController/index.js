a33e7411ef7b9a6e23e157661db67c084182a427
Rst(Rohit) Tiwari <rohittiwari@hkbdevelopment.com>
2023-12-29 17:32:47 +0530

    added inddex.js  for auth admin

---
+++ server/controller/authController/index.js
@@ -0,0 +1,14 @@
+import verify from "./verify.js"
+import register from "./register.js"
+
+
+/**
+ * From here each of Middleware function are being called 
+ * name of Data bases will be passed from here only
+ */
+const authMidleware = {
+
+}
+
+
+export default authMidleware
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express"
 import register from "../controller/authController/register.js"
 import verify from "../controller/authController/verify.js"
+import authMidleware from "../controller/authController/index.js"
 
 const router = express.Router()
 
