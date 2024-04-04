168403c501db6c7122777db0c97164b7373921ac
Rst(Rohit) Tiwari <rohittiwari@hkbdevelopment.com>
2024-04-04 17:17:47 +0530

    Conflict resolved

--- server/index.js
+++ server/index.js
@@ -62,13 +62,9 @@ cloudinary.config({
 });
 console.log(process.env.MDURL);
 mongoose
-<<<<<<< Updated upstream
   .connect(process.env.MDURL, {
     useUnifiedTopology: true,
   })
-=======
-  .connect("mongodb://myfac8ry:2121@ac-z992smf-shard-00-00.fmmrhks.mongodb.net:27017,ac-z992smf-shard-00-01.fmmrhks.mongodb.net:27017,ac-z992smf-shard-00-02.fmmrhks.mongodb.net:27017/myfac8ry?ssl=true&replicaSet=atlas-piyukq-shard-0&authSource=admin&retryWrites=true&w=majority")
->>>>>>> Stashed changes
   .then(() => {
     // Create HTTP or HTTPS server based on environment
     if (process.env.NODE_ENV === "production") {
